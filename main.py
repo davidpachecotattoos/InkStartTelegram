4c3bf7509ed42f1a65d6722f05688cc68676822b
davidpachecotattoos <davidpachecotattoos@gmail.com>
2025-08-02 11:42:01 +0200

    Update main.py
    Fix porta Flask per Render

--- main.py
+++ main.py
@@ -54,4 +54,5 @@ def send_telegram_message(chat_id, message):
     requests.post(TELEGRAM_URL, json=payload)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
